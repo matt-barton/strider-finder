05555dc88083987f60aa8ca9ea14f10a2e4430db
Matt Barton <matt@mattbarton.org>
2017-07-06 00:23:17 +0100

    added about and contact pages

--- client/index.js
+++ client/index.js
@@ -1,12 +1,30 @@
 const express = require('express'),
   app = express(),
-  template = require('jade').compileFile('./client/src/templates/homepage.jade');
+  jade = require('jade');
 
 app.use(express.static('client/public'));
 
 app.get('/', (req, res, next) => {
   try {
-    var html = template({ title: 'Home' });
+    var html = jade.compileFile('./client/src/templates/homepage.jade')({ title: 'Home' });
+    res.send(html);
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.get('/about', (req, res, next) => {
+  try {
+    var html = jade.compileFile('./client/src/templates/about.jade')({ title: 'About' });
+    res.send(html);
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.get('/contact', (req, res, next) => {
+  try {
+    var html = jade.compileFile('./client/src/templates/contact.jade')({ title: 'Contact' });
     res.send(html);
   } catch (e) {
     next(e);
